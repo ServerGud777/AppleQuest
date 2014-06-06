5530b5babbc4fd2e878b0c07f58f56d74230450e
JoshuaGud777 <joshgud777@gmail.com>
2014-06-06 13:08:33 -0600

    Changed name of dir

---
+++ Apple Quest/AppleQuest.bat
@@ -0,0 +1,42 @@
+:: Please open this file with eather code pade 473 or oem 850 to see the right symbles.
+:: https://github.com/JoshuaGud777/AppleQuest
+::
+::
+::
+@echo off
+:Menu
+cls
+echo ษอออออออออออออออออออออออป
+echo บ Welcome to AppleQuest บ
+echo ฬอออออออออออออออออออออออน
+echo บ 1. Login              บ
+echo บ 2. Create a Character บ
+echo บ 3. Options            บ
+echo บ 4. Dev. Contact info  บ
+echo บ 5. Not actualy an     บ
+echo บ option. Just needed   บ
+echo บ this to look nice. :) บ
+echo ศอออออออออออออออออออออออผ
+set /p tmpChoice="Please select a number then press Enter: "
+
+if %tmpChoice% LEQ 5 goto %tmpChoice%
+echo.
+echo Sorry that is not a valid option.
+ping -n 3 0.0.0.0 >nul
+goto :Menu
+
+
+:1
+echo 1
+:2
+echo 2
+:3
+echo 3
+:4
+echo 4
+:5
+echo 5
+:done
+echop done
+
+pause
